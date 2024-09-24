92d21ceef42ddc0485540bf9692ace9a47fc6c56
Sambath Prak <sambathprak789@gmail.com>
2024-09-24 13:30:34 +0700

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Hello from the github server 👋");
+});
+
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => {
+  console.log(`listen on port ${PORT}`);
+});
