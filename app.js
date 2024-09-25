95b90b6209107b8dc352d90d3b1c792370a98950
Sambath Prak <sambathprak789@gmail.com>
2024-09-25 14:08:09 +0700

    config morgan

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+app.use(morgan('dev'));
 app.use(express.json());
 // serve static files
 app.use(express.static(`${__dirname}/public`));
-
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const TOURS_ENDPOINT = '/api/v1/tours';
 const USERS_ENDPOINT = '/api/v1/users';
-
 
 app.use(TOURS_ENDPOINT, tourRouter);
 app.use(USERS_ENDPOINT, userRouter);
